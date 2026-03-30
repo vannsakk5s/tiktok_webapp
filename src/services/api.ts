a8b606882590d2e41dff9d274f468c8aec55f8de
zakk125 <vannzakk@gmail.com>
2026-03-31 00:34:12 +0700

    add s

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import { ApiPreviewResult } from '../types/tiktok';
 
-const API_BASE = 'http://tiktokdownloadskkh.duckdns.org/api/tiktok';
+const API_BASE = 'https://tiktokdownloadskkh.duckdns.org/api/tiktok';
 
 export const fetchPreview = async (url: string): Promise<ApiPreviewResult> => {
   try {
